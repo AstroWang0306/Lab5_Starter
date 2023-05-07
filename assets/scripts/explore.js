3a7c7e9173b2fdc41ea94833e5f2cd25b1311178
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2023-05-07 15:53:46 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -26,25 +26,3 @@ function init() {
     // Update the sound file based on the selected horn
     audioElement.src = 'assets/audio/' + hornSelection + '.mp3';
 }
-  
-function updateVolume() {
-    var volumeLevel = Number(volumeSlider.value);
-
-    // Update the volume level for the audio element
-    audioElement.volume = volumeLevel / 100;
-
-    // Update the volume icon based on the volume level
-    if (volumeLevel == 0) {
-      volumeIcon.src = 'assets/icons/volume-level-0.svg';
-      volumeIcon.alt = 'Volume Level 0';
-    } else if (volumeLevel < 33) {
-      volumeIcon.src = 'assets/icons/volume-level-1.svg';
-      volumeIcon.alt = 'Volume Level 1';
-    } else if (volumeLevel < 67) {
-      volumeIcon.src = 'assets/icons/volume-level-2.svg';
-      volumeIcon.alt = 'Volume Level 2';
-    } else {
-      volumeIcon.src = 'assets/icons/volume-level-3.svg';
-      volumeIcon.alt = 'Volume Level 3';
-    }
-  }

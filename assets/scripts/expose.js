89f8131eaa7808880fb36043bc29857388b322e1
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2023-05-07 17:40:37 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -22,6 +22,12 @@ function init() {
     
     if (hornSelect.value = "air-horn") {
       image.src = "assets/images/air-horn.svg";
+    } else if (hornSelect.value = "car-horn") {
+      image.src = "assets/images/car-horn.svg";
+    } else if (hornSelect.value = "party-horn") {
+      image.src = "assets/images/party-horn.svg";
+    } else {
+      image.src = "assets/images/no-image.png";
     }
     
     //document.querySelector("#expose img").src = "assets/images/${selectedHorn}.svg";

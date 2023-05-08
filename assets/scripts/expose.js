484d86e10bf08ed2e6e04f230c7c062661be3f49
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2023-05-07 18:06:28 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,33 +5,32 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   const hornSelect = document.getElementById("horn-select");
   const image = document.querySelector("#expose img");
-  //const volume = document.getElementById('volume');
-  //const volumeIcon = document.querySelector('#volume-controls img');
-  //const playButton = document.querySelector('#expose button');
-  //const audio = document.querySelector('.hidden');
+  const volume = document.getElementById("volume");
+  const volumeIcon = document.querySelector("#volume-controls img");
+  const playButton = document.querySelector("#expose button");
+  const audio = document.querySelector(".hidden");
   
   hornSelect.addEventListener("change", updateHorn);
-  // volume.addEventListener('input', updateVolume);
-  // playButton.addEventListener('click', playSound);
-  
-
+  volume.addEventListener("input", updateVolume);
+  playButton.addEventListener("click", playSound);
   
   function updateHorn() {
-    const selectedHorn = hornSelect.value;
-    //image.src = "assets/images/${selectedHorn}.svg";
-    
     if (hornSelect.value == "air-horn") {
-      document.querySelector("#expose img").src = "assets/images/air-horn.svg";;
+      image.src = "assets/images/air-horn.svg";
+      audio.src = "assets/audio/air-horn.mp3";
     } else if (hornSelect.value == "car-horn") {
-      document.querySelector("#expose img").src = "assets/images/car-horn.svg";
+      image.src = "assets/images/car-horn.svg";
+      audio.src = "assets/audio/car-horn.mp3";
     } else if (hornSelect.value == "party-horn") {
       image.src == "assets/images/party-horn.svg";
+      audio.src = "assets/audio/party-horn.mp3";
     } else {
       image.src == "assets/images/no-image.png";
     }
-    
-    //document.querySelector("#expose img").src = "assets/images/${selectedHorn}.svg";
-    
+  }
+  
+  function updateVolume() {
+  
   }
 }
  

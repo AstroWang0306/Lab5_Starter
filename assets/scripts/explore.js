1a1ce1f41f829964922cd4f68751d23114ba316c
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2023-05-07 18:47:47 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,28 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  //todo
+  const image = document.querySelector("#explore img");
+  const textToSpeak = document.getElementById("text-to-speak");
+  const voiceSelect = document.getElementById("voice-select");
+  const audio = document.querySelector("#explore button");
+ 
+  const synth = window.speechSynthesis;
+  
+  let voices = [];
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = "${voices[i].name} (${voices[i].lang})";
+
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
 }
